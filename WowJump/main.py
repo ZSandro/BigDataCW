7ac4b5a54981bcc77581e1272da93f9bd3e138d8
Xiang Zhao <ml203xz@leeds.ac.uk>
2022-04-20 23:30:21 +0100

    555

--- WowJump/main.py
+++ WowJump/main.py
@@ -1,6 +1,7 @@
 import os
 import Properties
-import threading
+from os import path
+import pandas
 
 
 def changePars(alpha, beta):
@@ -12,18 +13,44 @@ def changePars(alpha, beta):
     p['weightsString'] = new_par
     p.store(open(properties_path, 'w'))
 
+# 定义一个函数
+def scaner_file(url):
+    reMin = 9999
+    alpha = 0
+    beta = 0
+    # 遍历当前路径下所有文件
+    file = os.listdir(url)
+    for f in file:
+        # 字符串拼接
+        if f==".DS_Store":
+            continue
+        real_url = path.join(url, f)
+        df = pandas.read_csv(real_url + "/global-cost.csv")
+        df_par = pandas.read_csv(real_url + "/weights-alpha-beta.csv")
+        df.min()
+        # 打印出来
+        tmin = findMin(df.min()["Run-0"],df.min()["Run-1"],df.min()["Run-2"],df.min()["Run-3"],df.min()["Run-3"])
+        if tmin < reMin:
+            reMin = df.min()["Run-0"]
+            alpha = df_par["Unfairness weight"][0]
+            beta = df_par["Local cost weight"][0]
+        print(reMin, alpha, beta)
+
+
+def findMin(a,b,c,d,e):
+    return min(e, min(min(a, b), min(c, d)))
 
 if __name__ == '__main__':
-    command = "cd ../EPOS-jar && java -jar IEPOS-Tutorial.jar"
-    changePars(0, 0.3)
-    count = 0
-    for i in range(100):
-        for j in range(100):
-            alph = float((i+1) / 10)
-            beta = float((j+1) / 10)
-            if alph+beta < 1:
-                changePars(alph, beta)
-                os.system(command)
-    print(count)
-
+    # command = "cd ../EPOS-jar && java -jar IEPOS-Tutorial.jar"
+    # changePars(0, 0.3)
+    # count = 0
+    # for i in range(100):
+    #     for j in range(100):
+    #         alph = float((i+1) / 10)
+    #         beta = float((j+1) / 10)
+    #         if alph+beta < 1:
+    #             changePars(alph, beta)
+    #             os.system(command)
+    # print(count)
+    scaner_file("../EPOS-jar/output")
 
