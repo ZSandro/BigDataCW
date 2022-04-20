53fef46ecf25857360640f2eaba83ff6ba449f55
Xiang Zhao <ml203xz@leeds.ac.uk>
2022-04-20 23:31:28 +0100

    222

--- WowJump/main.py
+++ WowJump/main.py
@@ -13,22 +13,18 @@ def changePars(alpha, beta):
     p['weightsString'] = new_par
     p.store(open(properties_path, 'w'))
 
-# 定义一个函数
 def scaner_file(url):
     reMin = 9999
     alpha = 0
     beta = 0
-    # 遍历当前路径下所有文件
     file = os.listdir(url)
     for f in file:
-        # 字符串拼接
         if f==".DS_Store":
             continue
         real_url = path.join(url, f)
         df = pandas.read_csv(real_url + "/global-cost.csv")
         df_par = pandas.read_csv(real_url + "/weights-alpha-beta.csv")
         df.min()
-        # 打印出来
         tmin = findMin(df.min()["Run-0"],df.min()["Run-1"],df.min()["Run-2"],df.min()["Run-3"],df.min()["Run-3"])
         if tmin < reMin:
             reMin = df.min()["Run-0"]
