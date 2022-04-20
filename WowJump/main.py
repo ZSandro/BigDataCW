143031683bb7e949177ce09c1171428acef12985
Xiang Zhao <ml203xz@leeds.ac.uk>
2022-04-20 22:15:26 +0100

    1

--- WowJump/main.py
+++ WowJump/main.py
@@ -6,10 +6,9 @@ import Properties
 
 
 def changePars(alpha, beta):
-    file_path = "/Users/zhaoxiang/Desktop/Class/big data/CW/EPOS-jar/conf/epos.properties"
     new_par = "\"" + str(alpha) + " ," + str(beta) + "\""
 
-    properties_path = '/Users/zhaoxiang/Desktop/Class/big data/CW/EPOS-jar/conf/epos.properties'
+    properties_path = '../EPOS-jar/conf/epos.properties'
     p = Properties.Properties()
     p.load(open(properties_path))
     p['weightsString'] = new_par
@@ -17,13 +16,12 @@ def changePars(alpha, beta):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    path = "/Users/zhaoxiang/Desktop/Class/big\ data/CW/EPOS-jar"
-    command = "cd /Users/zhaoxiang/Desktop/EPOS-jar && java -jar IEPOS-Tutorial.jar"
+    command = "cd ../EPOS-jar && java -jar IEPOS-Tutorial.jar"
     changePars(0, 0)
 
-    for i in range(3):
-        beta = float((i+1) / 10)
-        changePars(0.3, beta)
-        os.system(command)
+    # for i in range(3):
+        # beta = float((i+1) / 10)
+        # changePars(0.3, beta)
+    os.system(command)
 
 
