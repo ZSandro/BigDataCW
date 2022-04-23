4c10b6d848b4a2d85ea8b106920aae15b9d7d02d
Xiang Zhao <ml203xz@leeds.ac.uk>
2022-04-23 21:29:34 +0100

    111

--- WowJump/main.py
+++ WowJump/main.py
@@ -14,9 +14,7 @@ def changePars(alpha, beta):
     p.store(open(properties_path, 'w'))
 
 def scaner_file(url):
-    reMin = 9999
-    alpha = 0
-    beta = 0
+    reMin = 999
     count = 0
     file = os.listdir(url)
     file_handle = open('../result.txt', mode='w')
@@ -30,7 +28,12 @@ def scaner_file(url):
             df_par = pandas.read_csv(real_url + "/weights-alpha-beta.csv")
         except:
             file_handle.write(real_url + "   error" + ' \n')
-        tmin = findMin(df.min()["Run-0"],df.min()["Run-1"],df.min()["Run-2"],df.min()["Run-3"],df.min()["Run-3"])
+        tmin = 9999
+        for row in df.min().iteritems():
+            if (row[0] == "Mean") or (row[0] == "Stdev") or (row[0] == "Iteration"):
+                continue
+            if tmin > row[1]:
+                tmin = row[1]
         if tmin < reMin:
             reMin = tmin
             alpha = df_par["Unfairness weight"][0]
@@ -47,13 +50,15 @@ def findMin(a,b,c,d,e):
 if __name__ == '__main__':
     command = "cd ../EPOS-jar && java -jar IEPOS-Tutorial.jar"
     count = 0
-    for i in range(1000):
-        for j in range(1000):
-            alph = float(i / 100)
-            beta = float(j / 100)
-            if alph+beta < 1:
-                changePars(alph, beta)
-                os.system(command)
-    print(count)
+    # for i in range(1000):
+    #     for j in range(1000):
+    #         alph = float(i / 100)
+    #         beta = float(j / 100)
+    #         if (alph < 0.009) & (beta < 0.009):
+    #             changePars(alph, beta)
+    #             os.system(command)
+    # print(count)
+    # changePars(0.008, 0.007)
+    # os.system(command)
     scaner_file("../EPOS-jar/output")
 
